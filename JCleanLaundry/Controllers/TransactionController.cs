264ee961bfa2710e7d5cc416a07c3d5af709444a
daniel panjaitan <daniel panjaitan@DESKTOP-J8BT6D8>
2016-11-05 15:38:42 +0700

    CAScout

--- JCleanLaundry/Controllers/TransactionController.cs
+++ JCleanLaundry/Controllers/TransactionController.cs
@@ -10,78 +10,40 @@ namespace JCleanLaundry.Controllers
             return View();
         }
 
-        /*
-        // GET: Transaction/Details/5
-        public ActionResult Details(int id)
+        [HttpGet]
+        public ActionResult CuciBaru()
         {
             return View();
         }
 
-        // GET: Transaction/Create
-        public ActionResult Create()
+        [HttpGet]
+        public ActionResult CariCucian()
         {
             return View();
         }
 
-        // POST: Transaction/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Transaction/Edit/5
-        public ActionResult Edit(int id)
+        [HttpGet]
+        public ActionResult Pengecekan()
         {
             return View();
         }
 
-        // POST: Transaction/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Transaction/Delete/5
-        public ActionResult Delete(int id)
+        [HttpGet]
+        public ActionResult Laporan()
         {
             return View();
         }
 
-        // POST: Transaction/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        [HttpGet]
+        public ActionResult TambahBarang()
         {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            return View();
         }
-        */
+
+        [HttpGet]
+        public ActionResult Pelanggan()
+        {
+            return View();
+        }
     }
 }
