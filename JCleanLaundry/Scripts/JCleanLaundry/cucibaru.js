854abc4a2b54c01dda4c7c7dcd15becf85644e6d
daniel panjaitan <daniel panjaitan@DESKTOP-J8BT6D8>
2016-11-06 12:06:37 +0700

    Ubah script cari pelanggan

--- JCleanLaundry/Scripts/JCleanLaundry/cucibaru.js
+++ JCleanLaundry/Scripts/JCleanLaundry/cucibaru.js
@@ -7,10 +7,15 @@
         contentType: "application/json; charset=utf-8",
         success: function(returnValue) {
             if (returnValue.noHpAda) {
-                _masukkanDataPelanggan(returnValue.pelanggan);
+                _pelanggan_dataPelangganAktif(false);
+                _pelanggan_aktifkanTombolPeriksaNoHp(false);
+                _pelanggan_noHpAktif(false);
+                _pelanggan_masukkanDataPelanggan(returnValue.pelanggan);
             } else {
-                _aktifkanpelanggan();
-                $("#cuci-baru_btn-periksa-pelanggan").attr("disabled", true);
+                _pelanggan_tampilkanerror(true);
+                _pelanggan_dataPelangganAktif(true);
+                _pelanggan_aktifkanTombolPeriksaNoHp(false);
+                _pelanggan_noHpAktif(true);
             }
         }
     };
@@ -18,33 +23,52 @@
     $.ajax(ajaxOpsi);
 }
 
-var _masukkanDataPelanggan = function(pelanggan) {
+var cucibaru_pelanggan_awal = function() {
+    _pelanggan_dataPelangganAktif(false);
+    _pelanggan_aktifkanTombolPeriksaNoHp(true);
+    _pelanggan_noHpAktif(true);
+    _pelanggan_tampilkanerror(false);
+}
+
+var _pelanggan_masukkanDataPelanggan = function (pelanggan) {
     $("#Pelanggan_Nama").val(pelanggan.Nama);
     $("#Pelanggan_NoKtp").val(pelanggan.NoKtp);
     $("#Pelanggan_Alamat").val(pelanggan.Alamat);
 }
 
-var cucibaru_nonaktifkanpelanggan = function() {
-    $("#Pelanggan_Nama").attr("disabled", true);
-    $("#Pelanggan_NoKtp").attr("disabled", true);
-    $("#Pelanggan_Alamat").attr("disabled", true);
+var _pelanggan_dataPelangganAktif = function(aktif) {
+    if (aktif) {
+        $("#Pelanggan_Nama, #Pelanggan_NoKtp, #Pelanggan_Alamat").removeAttr("disabled");
+    } else {
+        $("#Pelanggan_Nama, #Pelanggan_NoKtp, #Pelanggan_Alamat").attr("disabled", true);
+    }
 
-    $("#cuci-baru_btn-periksa-pelanggan").removeAttr("disabled");
-    $("#cuci-baru_btn-ulang-cari-pelanggan").attr("disabled", true);
-
-    $("#cuci-baru_div-pelanggan-error").hide();
+    $("#Pelanggan_Nama, #Pelanggan_NoKtp, #Pelanggan_Alamat").val("");
 }
 
-var _aktifkanpelanggan = function () {
-    $("#cuci-baru_div-pelanggan-error").show();
+var _pelanggan_aktifkanTombolPeriksaNoHp = function(aktif) {
+    if (aktif) {
+        $("#cuci-baru_btn-periksa-pelanggan").removeAttr("disabled");
+        $("#cuci-baru_btn-ulang-cari-pelanggan").attr("disabled", true);
+    } else {
+        $("#cuci-baru_btn-periksa-pelanggan").attr("disabled", true);
+        $("#cuci-baru_btn-ulang-cari-pelanggan").removeAttr("disabled");
+    }
+}
 
-    $("#Pelanggan_Nama").removeAttr("disabled");
-    $("#Pelanggan_NoKtp").removeAttr("disabled");
-    $("#cuci-baru_btn-ulang-cari-pelanggan").removeAttr("disabled");
+var _pelanggan_noHpAktif = function(aktif) {
+    if (aktif) {
+        $("#Pelanggan_Hp").val("");
+        $("#Pelanggan_Hp").removeAttr("disabled");
+    } else {
+        $("#Pelanggan_Hp").attr("disabled", true);
+    }
+}
 
-    $("#Pelanggan_Alamat").removeAttr("disabled");
-
-    $("#Pelanggan_Nama").val("");
-    $("#Pelanggan_NoKtp").val("");
-    $("#Pelanggan_Alamat").val("");
+var _pelanggan_tampilkanerror = function(aktif) {
+    if (aktif) {
+        $("#cuci-baru_div-pelanggan-error").show();
+    } else {
+        $("#cuci-baru_div-pelanggan-error").hide();
+    }
 }
